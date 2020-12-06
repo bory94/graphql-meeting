b67fe1b1ceda5cfd51ca4c19d8c94bdff7eb2a8b
Steve Katra <steve.katra@gmail.com>
2020-12-05 23:26:34 -0500

    GraphQL Meeting API with GraphiQL documentation

---
+++ src/test/java/com/stevekatra/graphqlmeeting/GraphqlMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevekatra.graphqlmeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
